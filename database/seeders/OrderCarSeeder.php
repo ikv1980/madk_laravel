59b78aa33c3a4624849d0b032d95b7aeae72574b
Konstantin <ikv1980@gmail.com>
2025-05-27 20:53:15 +0300

    Поправил сидер

--- database/seeders/OrderCarSeeder.php
+++ database/seeders/OrderCarSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\OrderCar;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -18,6 +19,13 @@ class OrderCarSeeder extends Seeder
 
         // Создаем данные через фабрику, но не сохраняем сразу
         $data = OrderCar::factory(20)->make()->toArray();
+
+        // Добавляем created_at и updated_at в каждую запись
+        $now = Carbon::now();
+        foreach ($data as &$row) {
+            $row['created_at'] = $now;
+            $row['updated_at'] = $now;
+        }
 
         // Вставляем все записи, игнорируя дубликаты
         DB::connection(env('CONNECTION_FOR_SEED'))
